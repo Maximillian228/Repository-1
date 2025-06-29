c4eab928271102eb0609a3ce69a3aa98c656e427
Maximillian228 <martuip300@gmail.com>
2025-06-29 22:31:37 +0300

    Delete day03_ex05.sql

--- day03_ex05.sql
+++
@@ -1,8 +0,0 @@
-SELECT pizzeria.name AS pizzeria_name
-FROM person_visits
-JOIN person ON pv.person_id = person.id
-JOIN pizzeria ON pv.pizzeria_id = pizzeria.id
-LEFT JOIN person_order ON po.person_id = person.id
-  JOIN menu ON po.menu_id = menu.id AND m.pizzeria_id = pizzeria.id
-WHERE person.name = 'Andrey' AND po.menu_id IS NULL
-ORDER BY pizzeria.name
